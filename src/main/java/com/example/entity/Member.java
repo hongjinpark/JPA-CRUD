030d3c90cd20b1adbdd40e532c69fbe694a6acc1
ParkHongJin <ektkfk180@naver.com>
2021-12-21 18:48:10 +0900

    Member 엔티티 추가
    어노테이션 추가

--- src/main/java/com/example/entity/Member.java
+++ src/main/java/com/example/entity/Member.java
@@ -1,7 +1,10 @@
 package com.example.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,9 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 
     @Id
